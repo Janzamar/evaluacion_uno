2d2f6474c7207be958a3f0e30617f4a968544d44
Janzamar <jazamar@lcg.unam.mx>
2025-04-03 22:25:20 -0600

    Código terminado y probado

--- bin/Ejercicio_9.py
+++ bin/Ejercicio_9.py
@@ -1,3 +1,8 @@
 # Ejericio 9
 # transformar secuencias de DNA a RNA
 
+secuencias = input("Dame secuencias separadas por comas, por favor\n").split(", ")
+
+secuencias_rna =[secuencia.replace ("T","U") for secuencia in secuencias]
+
+print(secuencias_rna)
