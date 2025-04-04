f416cb7df0fef9b4a31c70507e7c625d2098882a
Janzamar <jazamar@lcg.unam.mx>
2025-04-03 22:45:53 -0600

    ejercicio_12 terminado y probado

--- bin/Ejercicio_12.py
+++ bin/Ejercicio_12.py
@@ -1,3 +1,7 @@
 #Ejercicio 12
 # Encontrar secuencias que contengan un codón de paro
 
+secuencias = input("Dame secuencias separadas por comas, por favor\n").split(", ")
+
+secuencias_paro =[secuencia for secuencia in secuencias if "TAA" in secuencia or "TAG" in secuencia or "TGA" in secuencia]
+print(secuencias_paro)
